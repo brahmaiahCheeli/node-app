06838675ef08ae29372920ba7470478c66de9c28
Ubuntu <ubuntu@ip-172-31-21-227.ec2.internal>
2021-02-07 08:45:11 +0000

    modified index js

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ app.get('/', function (req, res) {
 });
 //listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
- 
+console.log('Server started and listening at 3000 port..');  
 module.exports = app;
